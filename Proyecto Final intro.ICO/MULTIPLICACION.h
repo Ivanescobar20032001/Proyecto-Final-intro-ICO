1a9dc05f7316112ad12057e5a1f3f659f40d3b65
Ivanescobar20032001 <aaaaaalanivan@gmail.com>
2021-12-20 16:32:55 -0600

    Se hico un cambio en MULTIPLICACION.h

---
+++ Proyecto Final intro.ICO/MULTIPLICACION.h
@@ -0,0 +1,9 @@
+// MULTIPLICACIÓN.cpp : Este archivo contiene la función "main". 
+//
+
+int multi(int a, int b);
+
+int multi(int a, int b)
+{
+    return (a * b);
+}
--- Proyecto Final intro.ICO/MULTIPLICACIÓN.h
+++
@@ -1,9 +0,0 @@
-// MULTIPLICACIÓN.cpp : Este archivo contiene la función "main". 
-//
-
-int multi(int a, int b);
-
-int multi(int a, int b)
-{
-    return (a * b);
-}
